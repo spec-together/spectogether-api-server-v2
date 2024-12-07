3e407a01a56d4d254d30eed4bfaa295d7a369d61
JayOneC <xprs42@gmail.com>
2024-12-07 09:20:47 +0900

    Fix: typo in filename

--- models/define/contest_calandar.js
+++ models/define/contest_calandar.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require("sequelize");
 
 class ContestCalendar extends Model {
   static init(sequelize) {
-    super.init(
+    return super.init(
       {
         contest_calendar_id: {
           type: DataTypes.BIGINT,
