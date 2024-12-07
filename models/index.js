4bf6638b4817674734d2213b5587a9324551ed5f
JayOneC <xprs42@gmail.com>
2024-12-07 09:21:50 +0900

    Feat: model 초기화를 일괄 처리로 변경

--- models/index.js
+++ models/index.js
@@ -3,42 +3,6 @@ const { MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_PORT, MYSQL_USER } =
   require("../config.json").DATABASE;
 const logger = require("../logger");
 
-const User = require("./define/user");
-const Term = require("./define/term");
-const Area = require("./define/area");
-const Spec = require("./define/spec");
-const EmailVerificationCode = require("./define/email_verification_code");
-const Calendar = require("./define/calendar");
-const Contest = require("./define/contest");
-const StudyroomVideocall = require("./define/studyroom_videocall");
-const UserTerm = require("./define/user_term");
-const UserSpec = require("./define/user_spec");
-const UserArea = require("./define/user_area");
-const UserOauth = require("./define/user_oauth");
-const UserVerifiedEmail = require("./define/user_verified_email");
-const UserRefreshToken = require("./define/user_refresh_token");
-const Studyroom = require("./define/studyroom");
-const UserStudyroom = require("./define/user_studyroom");
-const StudyroomMember = require("./define/studyroom_member");
-const StudyroomCalendar = require("./define/studyroom_calendar");
-const Schedule = require("./define/schedule");
-const ScheduleParticipant = require("./define/schedule_participant");
-const ContestCalendar = require("./define/contest_calendar");
-const ContestSchedule = require("./define/contest_schedule");
-const UserCalendar = require("./define/user_calendar");
-const Todo = require("./define/todo");
-const StudyroomTodo = require("./define/studyroom_todo");
-const TodoMember = require("./define/todo_member");
-const ContestQuestion = require("./define/contest_question");
-const ContestAnswer = require("./define/contest_answer");
-const Board = require("./define/board");
-const ContestBoard = require("./define/contest_board");
-const Inquiry = require("./define/inquiry");
-const InquiryAnswer = require("./define/inquiry_answer");
-const StudyroomVideocallMember = require("./define/studyroom_videocall_member");
-const StudyroomChat = require("./define/studyroom_chat");
-const KakaoUserInfo = require("./define/kakao_user_info");
-
 // Sequelize 인스턴스 생성
 const sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, {
   host: MYSQL_HOST,
@@ -54,49 +18,26 @@ const sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, {
   },
 });
 
-const models = {
-  User: User.init(sequelize),
-  Term: Term.init(sequelize),
-  Area: Area.init(sequelize),
-  Spec: Spec.init(sequelize),
-  EmailVerificationCode: EmailVerificationCode.init(sequelize),
-  Calendar: Calendar.init(sequelize),
-  Contest: Contest.init(sequelize),
-  StudyroomVideocall: StudyroomVideocall.init(sequelize),
-  UserTerm: UserTerm.init(sequelize),
-  UserSpec: UserSpec.init(sequelize),
-  UserArea: UserArea.init(sequelize),
-  UserOauth: UserOauth.init(sequelize),
-  UserVerifiedEmail: UserVerifiedEmail.init(sequelize),
-  UserRefreshToken: UserRefreshToken.init(sequelize),
-  Studyroom: Studyroom.init(sequelize),
-  UserStudyroom: UserStudyroom.init(sequelize),
-  StudyroomMember: StudyroomMember.init(sequelize),
-  StudyroomCalendar: StudyroomCalendar.init(sequelize),
-  Schedule: Schedule.init(sequelize),
-  ScheduleParticipant: ScheduleParticipant.init(sequelize),
-  ContestCalendar: ContestCalendar.init(sequelize),
-  ContestSchedule: ContestSchedule.init(sequelize),
-  UserCalendar: UserCalendar.init(sequelize),
-  Todo: Todo.init(sequelize),
-  StudyroomTodo: StudyroomTodo.init(sequelize),
-  TodoMember: TodoMember.init(sequelize),
-  ContestQuestion: ContestQuestion.init(sequelize),
-  ContestAnswer: ContestAnswer.init(sequelize),
-  Board: Board.init(sequelize),
-  ContestBoard: ContestBoard.init(sequelize),
-  Inquiry: Inquiry.init(sequelize),
-  InquiryAnswer: InquiryAnswer.init(sequelize),
-  StudyroomVideocallMember: StudyroomVideocallMember.init(sequelize),
-  StudyroomChat: StudyroomChat.init(sequelize),
-  KakaoUserInfo: KakaoUserInfo.init(sequelize),
-  Sequelize,
-  sequelize,
-};
+const path = require("node:path");
+const fs = require("node:fs");
 
-// 모델 간의 관계 설정
-Object.values(models)
-  .filter((model) => typeof model.associate === "function")
-  .forEach((model) => model.associate(models));
+const modelsDir = path.join(__dirname, "define");
+const modelFiles = fs.readdirSync(modelsDir);
 
-module.exports = models;
+const db = {};
+
+for (const file of modelFiles) {
+  const model = require(path.join(modelsDir, file)); // Function
+  model.init(sequelize);
+  db[model.name] = model; // string
+}
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+
+module.exports = db;
