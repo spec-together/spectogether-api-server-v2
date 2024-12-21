b2b3b49e9db39f223779ec69b11ad20a4fdf3d9f
JayOneC <xprs42@gmail.com>
2024-12-22 01:58:27 +0900

    Refactor: 이메일 인증 관련 리포지토리 삭제 및 서비스에서 직접 모델 사용으로 변경

--- services/verification/email.verification.service.js
+++ services/verification/email.verification.service.js
@@ -1,13 +1,17 @@
 const crypto = require("node:crypto");
+const { User, VerificationCode } = require("../../models/index.js");
 // const { Op } = require("sequelize");
 
-const emailVerificationRepo = require("../../repositories/email.verification.repository.js");
+// const emailVerificationRepo = require("../../repositories/email.verification.repository.js");
 const sendVerificationEmail = require("../../utils/mailer.util.js");
 const { AlreadyExistsError, InvalidTokenError } = require("../../errors.js");
 
 const isEmailUnique = async (email) => {
-  const user = await emailVerificationRepo.findUserByEmail(email);
-  if (user === null) {
+  const exUser = await User.findOne({
+    where: { email },
+    attributes: ["email", "user_id"],
+  });
+  if (exUser === null) {
     return true;
   } else {
     throw new AlreadyExistsError("이미 사용 중인 이메일입니다.");
@@ -20,28 +24,44 @@ const generateToken = () => {
 
 const sendVerification = async (email) => {
   const code = generateToken();
-  await emailVerificationRepo.saveEmailVerificationCode(email, code);
+  await VerificationCode.create({
+    identifier_type: "email",
+    identifier_value: email,
+    verification_code: code,
+  });
   await sendVerificationEmail(email, code);
   return code;
 };
 
 const verifyToken = async (email, code) => {
-  const record = await emailVerificationRepo.findByEmailAndCode(email, code);
+  const record = await VerificationCode.findOne({
+    where: {
+      identifier_type: "email",
+      identifier_value: email,
+      verification_code: code,
+    },
+  });
   if (!record) {
     throw new InvalidTokenError(
       "이메일 인증 실패. 유효하지 않은 인증 토큰입니다."
     );
   }
-  if (email !== record.email) {
+  if (email !== record.identifier_value) {
     throw new InvalidTokenError(
       "이메일 인증 실패. 유효하지 않은 이메일입니다."
     );
   }
-  await emailVerificationRepo.deleteEmailVerificationCode(code);
+  await VerificationCode.destroy({
+    where: {
+      identifier_type: "email",
+      identifier_value: email,
+      verification_code: code,
+    },
+  });
   // TODO : 지금은 유효하면 바로 삭제처리하고 FE에 인증 여부를 반환값으로 간접 제공. 인증 정보를 DB에 저장하는 방안도 고려해보자.
   return {
-    email_verification_code_id: record.email_verification_code_id,
-    email: record.email,
+    email_verification_code_id: record.verification_code_id,
+    email: record.identifier_value,
   };
 
   // TODO: 만료 시간 검증 추가
@@ -54,6 +74,8 @@ const verifyToken = async (email, code) => {
   //     "이메일 인증 실패. 인증 토큰이 만료되었습니다."
   //   );
   // }
+
+  // TODO : verifyToken 함수 호출 마다 시도 횟수 변경 처리 추가
 };
 
 module.exports = {
