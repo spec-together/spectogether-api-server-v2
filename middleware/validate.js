3cd0e91780032cf1503885905d0d78a32ad9bdde
JayOneC <xprs42@gmail.com>
2024-12-22 21:41:09 +0900

    Refactor: 유효성 검사 미들웨어에서 오류 처리 개선 및 사용자 정의 오류 클래스 사용

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,8 +1,16 @@
+const { InvalidInputError } = require("../errors");
+
 const validate = (schema) => {
   return (req, res, next) => {
     const valid = schema(req.body);
     if (!valid) {
-      return res.status(400).json({ errors: schema.errors });
+      const errorDetails = schema.errors.map((err) => ({
+        instancePath: err.instancePath,
+        message: err.message,
+      }));
+      return next(
+        new InvalidInputError("유효하지 않은 입력입니다.", errorDetails)
+      );
     }
     next();
   };
