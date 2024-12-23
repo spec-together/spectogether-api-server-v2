47d7cd0829664172786fdaa15945f61428ea6188
JayOneC <xprs42@gmail.com>
2024-12-23 20:23:36 +0900

    Feat: models/index.js 에서 모델 동적 로딩 제거

--- models/index.js
+++ models/index.js
@@ -5,7 +5,6 @@ const path = require("node:path");
 const Sequelize = require("sequelize");
 
 const logger = require("../logger");
-// const basename = path.basename(__filename);
 const dBconfig = require("../config.json").DATABASE;
 
 const sequelize = new Sequelize(
@@ -27,25 +26,52 @@ const sequelize = new Sequelize(
   }
 );
 
-const modelsDir = path.join(__dirname, "define");
-const modelFiles = fs.readdirSync(modelsDir);
-
 const db = {};
+db.Admin = require("./define/admin");
+db.Area = require("./define/area");
+db.Event = require("./define/event");
+db.EventImage = require("./define/event_image");
+db.Inquiry = require("./define/inquiry");
+db.InquiryAnswer = require("./define/inquiry_answer");
+db.Notice = require("./define/notice");
+db.NoticeImage = require("./define/notice_image");
+db.Organization = require("./define/organization");
+db.OrganizationUser = require("./define/organization_user");
+db.School = require("./define/school");
+db.Spec = require("./define/spec");
+db.SpecPhoto = require("./define/spec_photo");
+db.Studyroom = require("./define/studyroom");
+db.StudyroomChat = require("./define/studyroom_chat");
+db.StudyroomMember = require("./define/studyroom_member");
+db.StudyroomTodo = require("./define/studyroom_todo");
+db.Term = require("./define/term");
+db.Todo = require("./define/todo");
+db.TodoParticipant = require("./define/todo_participant");
+db.User = require("./define/user");
+db.UserArea = require("./define/user_area");
+db.UserLocal = require("./define/user_local");
+db.UserOauth = require("./define/user_oauth");
+db.UserPayments = require("./define/user_payments");
+db.UserPremium = require("./define/user_premium");
+db.UserRefreshToken = require("./define/user_refresh_token");
+db.UserSchool = require("./define/user_school");
+db.UserSpec = require("./define/user_spec");
+db.UserStudyroom = require("./define/user_studyroom");
+db.UserTerm = require("./define/user_term");
+db.UserTodo = require("./define/user_todo");
+db.VerificationCode = require("./define/verification_code");
+// console.log("모델 갯수 :", Object.keys(db).length);
 
-modelFiles
-  .filter((file) => {
-    return (
-      file.slice(-3) === ".js" &&
-      // file !== basename &&
-      !file.startsWith(".") &&
-      !file.endsWith(".test.js")
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(modelsDir, file)); // Function
-    model.init(sequelize);
-    db[model.name] = model;
-  });
+// const modelsDir = path.join(__dirname, "define");
+// const modelFiles = fs.readdirSync(modelsDir);
+// modelFiles.filter((file) => { return (file.slice(-3) === ".js" && !file.startsWith(".") && !file.endsWith(".test.js")); })
+//   .forEach((file) => { const model = require(path.join(modelsDir, file)); model.init(sequelize); db[model.name] = model; });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].init) {
+    db[modelName].init(sequelize);
+  }
+});
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
