f5acb7d9eacfea02dbaf1b94e27e27c7ae11bcb1
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-01-03 21:58:41 +0900

    Chore: minor changes in videocall

--- services/test.js
+++ services/test.js
@@ -1,3 +1,3 @@
 const { encrypt62, decrypt62 } = require("../utils/encrypt.util");
 
-console.log(decrypt62("XfJrrNMyKm1Bp2JN3poK27ljHH78db8dEA39JadSaPM"));
+console.log(encrypt62("XfJrrNMyKm1Bp2JN3poK27ljHH78db8dEA39JadSaPM"));
